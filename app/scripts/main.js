8b1453fd92caae130c0ad001fa42d1515d3b85f4
Travis Gorman <thefarbetween@me.com>
2016-12-10 14:23:15 -0600

    Name component with React.component

---
+++ app/scripts/main.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+let page = document.getElementById('page')
+
+// component - class X extends React.Component
+class Name extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Travis',
+    };
+  }
+
+  render () {
+    return (  
+      <div className="displayName">
+        <h1>Hi, my name is {this.state.name} </h1>
+      </div>
+    );
+  }
+};
+// component - React.createClass
+const NameClass = React.createClass({
+  getInitialState(){
+    return {
+      name: "Travis!",
+    };
+  },
+  render(){
+    return (  
+      <div>
+        <h1>
+          Hi, my name is {this.state.name}
+        </h1>
+      </div>
+    );
+  },
+});
+
+// dom
+ReactDOM.render(  
+  <NameClass />, page
+);
